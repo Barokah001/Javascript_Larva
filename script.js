0eb21e31aa7c72920a2238827126257fb76cb411
barokah001 <alimibarakat001@gmail.com>
2025-04-15 12:00:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// console.log('My name is Barokah Oyindamola');
+// console.log('Hiiii, I think today is going to be a great day!')
+// console.log('Java')
+// console.log('Barokah')
+// console.log("Barokah Oyin")
+// console.log(`Barokah Oyindamola`);
+// console.log(`Yo, what's today's date?`);
+// console.log('I\'m tired')
+// console.log(typeof(`what's up?`));
+// console.log(typeof(2.5));
+// console.log(typeof(false));
+// console.log(typeof(null));
+// console.log(2.5 + 7.3)
+// console.log('Barokah' + ' Oyindamola')
+// console.log(2**10)
+// console.log(3%2)
+// console.log(2%5)
+// console.log((3 + 1) * 2)
+
+
+console.log(true && true)  // true
+console.log(true && false);  // false
+console.log(false && true);  // false
+console.log(false && false); // false
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+
+console.log(!true) // false
+console.log(!false); // true
